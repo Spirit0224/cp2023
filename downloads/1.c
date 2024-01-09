77e82a1a517a21d189475293215a2af435b38e88
Spirit0224 <144768431+Spirit0224@users.noreply.github.com>
2024-01-09 02:21:27 +0000

    新增C.1

---
+++ downloads/1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()  
+{
+    // Print Name
+    printf("Name   : 李詮聖\n"); 
+
+    // Print Date of Birth
+    printf("DOB    : February 24, 2005\n"); 
+
+    // Print Mobile Number
+    printf("Mobile : 0966-676970\n"); 
+
+    // Indicate successful execution
+    return(0); 
+}
